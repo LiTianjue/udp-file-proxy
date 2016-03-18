5066aa331432e6f8491109de1d24a9aa4ba2ac14
andy <mhpmii@126.com>
2016-03-18 14:10:14 +0800

    添加安装脚本和可执行程序

--- TTst/Sudp/del_app.sh
+++ TTst/Sudp/del_app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Sending..."
-./delect_app.py appTest1
+#./delect_app.py appTest1
 ./delect_app.py appTest2
 #./register_app.py appTest3
--- install/install.sh
+++ install/install.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
-#设置的时候要区分服务端还是客户端
-# 增大接收缓冲区的大小
-#sysctl -w net.core.rmem_max=16777216
-#sysctl -w net.core.rmem_default=16777216
-
-# 增加接收队列的容量
-#sysctl -w kernel.msgmnb=16777216
-
-#sysctl -p
-
 # 创建应用目录
 mkdir /usr/share/udp-file-proxy  2>>./install.log
 mkdir /usr/share/udp-file-proxy/bin 2>>./install.log
 mkdir /usr/share/udp-file-proxy/etc 2>>./install.log
 mkdir /usr/share/udp-file-proxy/log	2>>./install.log
-
 cp -r  ./udp-file-proxy  /usr/share/ 2>>./install.log
+
+if [[ $1 == "server" ]]
+then
+	echo "Install for Server( File receive)"
+	#设置的时候要区分服务端还是客户端
+	# 增大接收缓冲区的大小
+	sysctl -w net.core.rmem_max=16777216
+	sysctl -w net.core.rmem_default=16777216
+	# 增加接收队列的容量
+	sysctl -w kernel.msgmnb=16777216
+	sysctl -p
+
+elif [[ $1 == "client" ]]
+then
+	echo "Install for Client( File Send)"
+	cp -f ./udp-file-proxy/etc/udp-file-proxy-client.cfg /usr/share/udp-file-proxy/etc/udp-file-proxy.cfg
+else
+	echo "Usage :"
+	echo "$0 client/server"
+	exit
+fi
+chmod +x /usr/share/udp-file-proxy/bin/*
+
+
+
